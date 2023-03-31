c72650e82fd88fe3b0e50fef56b796d6bf1cbda8
Sergii Ternytskyi <serhij86@gmail.com>
2023-03-31 22:45:08 +0300

    added lighttheme
    created theming and added the light theme

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+import "modern-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+    margin: 0;
+    }
+
+ul,
+ol {
+  margin: 0;
+  padding: 0;
+}
+
+img{ 
+  display: block;
+  max-width: 100%;
+  height: auto;
+  
+}
+
+body {
+  margin: 0;
+  font-family: ${(p) => p.theme.fonts.main};
+  font-size: ${(p) => p.theme.fontSizes.m}px;
+  font-weight: ${(p) => p.theme.fontWeights.regular};
+
+  background-color: ${(p) => p.theme.colors.backgroundPrimary};
+}
+`;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+
 import { App } from "./Components/App/App";
+import { themeLight } from "./themes/themeLight";
+import { GlobalStyle } from "./Components/GlobalStyles";
+// import { themeDark } from "./themes/themeDark";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={themeLight}>
+            <GlobalStyle />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
