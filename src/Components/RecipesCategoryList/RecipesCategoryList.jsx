08df8bb2c465bc3614c04139327f89845b073e25
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-04 00:20:42 +0300

    Update RecipesCategoryList.jsx

--- src/Components/RecipesCategoryList/RecipesCategoryList.jsx
+++ src/Components/RecipesCategoryList/RecipesCategoryList.jsx
@@ -1,13 +1,20 @@
+import { useLocation } from "react-router";
+
 import { RecipeCard } from "Components/RecipeCard/RecipeCard";
 import { ItemStyled, ListStyled } from "./RecipesCategoryList.styled";
+import { Link } from "react-router-dom";
 
 export const RecipesCategoryList = ({ recipes }) => {
+    const location = useLocation();
+
     return (
         <ListStyled>
             {recipes.map((recipe) => {
                 return (
                     <ItemStyled key={recipe._id}>
-                        <RecipeCard recipe={recipe} />
+                        <Link to={`${recipe._id}`} state={{ from: location }}>
+                            <RecipeCard recipe={recipe} />
+                        </Link>
                     </ItemStyled>
                 );
             })}
