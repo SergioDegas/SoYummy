86ca58ffc8b5233859c599991493a20ef9baa3c9
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2023-04-05 11:14:09 +0300

    Update RecipeIngredients.jsx

--- src/Components/AddRecipeForm/RecipeIngredients/RecipeIngredients.jsx
+++ src/Components/AddRecipeForm/RecipeIngredients/RecipeIngredients.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { GrClose } from "react-icons/gr";
 import { IoIosArrowDown } from "react-icons/io";
