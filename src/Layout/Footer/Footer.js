7ffb9b8e75d32f84b647296028858a3a0f22c33a
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-04-06 21:33:02 +0300

    completed footer
    Adaprive layout on footer and ready to use

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -26,69 +26,103 @@ import {
   InstIcon,
   SocialContainer,
   Link,
+  Copyright,
+  CopyrightText,
+  MenuContainer,
+  ContactBox,
 } from "./FooterStyled";
 
 const Footer = () => {
   return (
-    <FooterContainer>
-      <Container>
-      <MainContainer>
-        <InfoContainer>
-          <HeadContainer>
-            <LogoContainer>
-              <LogoIcon />
-            </LogoContainer>
-            <FooterTitle>So Yummy</FooterTitle>
-          </HeadContainer>
-          <BenefitsList>
-            <BenefitsItem>
-              <Dot>&#8226;</Dot>Database of recipes that can be replenished{" "}
-            </BenefitsItem>
-            <BenefitsItem>
-              <Dot>&#8226;</Dot>
-              Flexible search for desired and unwanted
-              <BenefitText>ingredients</BenefitText>
-            </BenefitsItem>
-            <BenefitsItem>
-              <Dot>&#8226;</Dot>Ability to add your own recipes with photos
-            </BenefitsItem>
-            <BenefitsItem>
-              <Dot>&#8226;</Dot>Convenient and easy to use
-            </BenefitsItem>
-          </BenefitsList>
-        </InfoContainer>
-        <MenuList>
-          <MenuItem><Link to='/' >Ingredients</Link></MenuItem>
-          <MenuItem><Link to='/add'>Add recipes</Link></MenuItem>
-          <MenuItem><Link to='/my'>My recipes</Link></MenuItem>
-          <MenuItem><Link to='/favorite'>Favorite</Link></MenuItem>
-          <MenuItem><Link to='/shopping-list'>Shopping list</Link></MenuItem>
-        </MenuList>
-        <div>
-        <EmailTitle>Subscribe to our Newsletter</EmailTitle>
-        <EmailText>Subscribe up to our newsletter. Be in touch with 
-          <br/>
-          latest news and special offers, etc.</EmailText>
-        <MailContainer>
-        <EmailInput 
-        type="email" 
-        placeholder="Enter your email address"
-        /> 
-        <MailIcon />
-        </MailContainer>  
-        <MailBtn type="submit">Subscribe</MailBtn>
-      </div>
-      </MainContainer>
-      
-      <SocialContainer>
-      <li><FacebookIcon/></li>
-      <li><YTIcon/></li>
-      <li><TwitterIcon/></li>
-      <li><InstIcon/></li>
-      </SocialContainer>
-      </Container>
-    </FooterContainer>
-   
+    <>
+      <FooterContainer>
+        <Container>
+          <MainContainer>
+            <MenuContainer>
+              <InfoContainer>
+                <HeadContainer>
+                  <LogoContainer>
+                    <LogoIcon />
+                  </LogoContainer>
+                  <FooterTitle>So Yummy</FooterTitle>
+                </HeadContainer>
+                <BenefitsList>
+                  <BenefitsItem>
+                    <Dot>&#8226;</Dot>Database of recipes that can be
+                    replenished{" "}
+                  </BenefitsItem>
+                  <BenefitsItem>
+                    <Dot>&#8226;</Dot>
+                    Flexible search for desired and unwanted
+                    <BenefitText> ingredients</BenefitText>
+                  </BenefitsItem>
+                  <BenefitsItem>
+                    <Dot>&#8226;</Dot>Ability to add your own recipes with
+                    photos
+                  </BenefitsItem>
+                  <BenefitsItem>
+                    <Dot>&#8226;</Dot>Convenient and easy to use
+                  </BenefitsItem>
+                </BenefitsList>
+              </InfoContainer>
+
+              <MenuList>
+                <MenuItem>
+                  <Link to="/">Ingredients</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/add">Add recipes</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/my">My recipes</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/favorite">Favorite</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/shopping-list">Shopping list</Link>
+                </MenuItem>
+              </MenuList>
+            </MenuContainer>
+            <ContactBox>
+              <EmailTitle>Subscribe to our Newsletter</EmailTitle>
+              <EmailText>
+                Subscribe up to our newsletter. Be in touch with
+                <br />
+                latest news and special offers, etc.
+              </EmailText>
+              <MailContainer>
+                <EmailInput
+                  type="email"
+                  placeholder="Enter your email address"
+                />
+                <MailIcon />
+              </MailContainer>
+              <MailBtn type="submit">Subscribe</MailBtn>
+            </ContactBox>
+          </MainContainer>
+
+          <SocialContainer>
+            <li>
+              <FacebookIcon />
+            </li>
+            <li>
+              <YTIcon />
+            </li>
+            <li>
+              <TwitterIcon />
+            </li>
+            <li>
+              <InstIcon />
+            </li>
+          </SocialContainer>
+        </Container>
+      </FooterContainer>
+      <Copyright>
+        &copy; 2023 All Rights Reserved.{" "}
+        <CopyrightText>Terms of Service</CopyrightText>
+      </Copyright>
+    </>
   );
 };
 
--- src/Layout/Footer/FooterStyled.js
+++ src/Layout/Footer/FooterStyled.js
@@ -21,28 +21,69 @@ export const Link = styled(NavLink)`
 
 export const FooterContainer = styled.div`
   background-color: ${({ theme }) => `${theme.colors.backgroundSecondary}`};
-  height: 420px;
+  height: 435px;
+  @media (min-width: 768px) {
+    height: 425px;
+  }
+  @media (min-width: 1240px) {
+    height: 420px;
+  }
   width: 100%;
 `;
 export const MainContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding-top: ${({ theme }) => `${theme.space[5]}px`};
+  padding-top: 28px;
+  display: block;
+  @media (min-width: 768px) {
+    padding-top: 50px;
+    display: block;
+  }
+  @media (min-width: 1240px) {
+    display: flex;
+    padding-top: ${({ theme }) => `${theme.space[5]}px`};
+    justify-content: space-between;
+  } ;
+`;
+export const MenuContainer = styled.div`
+  display: block;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-around;
+  }
+  @media (min-width: 1240px) {
+    display: flex;
+    gap: 120px;
+  } ;
 `;
 export const InfoContainer = styled.div``;
 export const HeadContainer = styled.div`
   display: flex;
-  align-items: flex-start;
-  gap: 12px;
+  justify-content: center;
+  gap: 8px;
+  margin-bottom: 32px;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: flex-start;
+    margin-bottom: 24px;
+  }
+  @media (min-width: 1240px) {
+    gap: 12px;
+    justify-content: flex-start;
+    margin-bottom: 0;
+  } ;
 `;
 export const LogoContainer = styled.div`
-  width: 44px;
-  height: 44px;
+  width: 32px;
+  height: 32px;
   background-color: ${({ theme }) => `${theme.colors.accent}`};
-  border-radius: 12px;
+  border-radius: 6px;
   display: flex;
   justify-content: center;
   align-items: center;
+  @media (min-width: 1240px) {
+    width: 44px;
+    height: 44px;
+    border-radius: 12px;
+  }
 `;
 
 export const LogoIcon = styled(GiEvilFork)`
@@ -51,20 +92,40 @@ export const LogoIcon = styled(GiEvilFork)`
   color: ${({ theme }) => `${theme.colors.mainLight}`};
 `;
 export const FooterTitle = styled.h1`
-  width: 159px;
-  height: 28px;
+  width: 102px;
+  height: 18px;
+  font-size: 18px;
+  line-height: ${({ theme }) => `${theme.lineHeights.single}`};
   font-family: ${({ theme }) => `${theme.fonts.main}`};
   font-style: normal;
   font-weight: ${({ theme }) => `${theme.fontWeights.bold}`};
-  font-size: 28px;
   line-height: ${({ theme }) => `${theme.lineHeights.single}`};
-  letter-spacing: 0.015em;
   color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
   margin-top: auto;
   margin-bottom: auto;
+  @media (min-width: 768px) {
+    width: 159px;
+    height: 28px;
+    font-size: 28px;
+    line-height: 28px;
+    letter-spacing: 0.015em;
+  }
+  @media (min-width: 1240px) {
+    width: 159px;
+    height: 28px;
+    font-size: 28px;
+    letter-spacing: 0.015em;
+  }
 `;
 export const BenefitsList = styled.ul`
-  margin-top: 40px;
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+  }
+  @media (min-width: 1240px) {
+    display: block;
+    margin-top: 40px;
+  }
 `;
 
 export const BenefitsItem = styled.li`
@@ -81,62 +142,130 @@ margin-bottom:12px;
 }
 }
 `;
+
 export const Dot = styled.span`
   margin-right: 5px;
 `;
 export const BenefitText = styled.span`
-  display: block;
-  text-indent: 13px;
+  @media (min-width: 768px) {
+    white-space: nowrap;
+  }
+  @media (min-width: 1240px) {
+    display: block;
+    text-indent: 13px;
+  }
 `;
 export const MenuList = styled.ul``;
+
 export const MenuItem = styled.li`
+display: block;
+text-align: center;
+&:not(:last-child) {
+    margin-bottom: 14px;
+  @media (min-width: 768px) {
+    margin-bottom: 20px;
+  }
+@media (min-width: 1240px) {
   &:not(:last-child) {
     margin-bottom: 24px;
+  };
+  }
+`;
+
+export const ContactBox = styled.div`
+  @media (min-width: 768px) {
+    gap: 12px;
+    margin-top: 72px;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 38px;
+  }
+  @media (min-width: 1240px) {
+    margin-top: 0;
+    display: block;
   }
 `;
 
 export const EmailTitle = styled.h2`
-  font-family: ${({ theme }) => `${theme.fonts.main}`};
-  font-style: normal;
-  font-weight: ${({ theme }) => `${theme.fontWeights.bold}`};
-  font-size: 18px;
-  line-height: ${({ theme }) => `${theme.lineHeights.large}`};
-  color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
-  margin-bottom: ${({ theme }) => `${theme.space[3]}px`};
+  display: none;
+  @media (min-width: 1240px) {
+    display: block;
+    &:not(:last-child) {
+      margin-bottom: 24px;
+      font-family: ${({ theme }) => `${theme.fonts.main}`};
+      font-style: normal;
+      font-weight: ${({ theme }) => `${theme.fontWeights.bold}`};
+      font-size: 18px;
+      line-height: ${({ theme }) => `${theme.lineHeights.large}`};
+      color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
+      margin-bottom: ${({ theme }) => `${theme.space[3]}px`};
+    }
+  }
 `;
 
 export const EmailText = styled.p`
-  font-family: ${({ theme }) => `${theme.fonts.main}`};
-  font-style: normal;
-  font-weight: ${({ theme }) => `${theme.fontWeights.regular}`};
-  font-size: 14px;
-  line-height: ${({ theme }) => `${theme.lineHeights.heading}`};
-  letter-spacing: -0.02em;
-  color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
-  margin-bottom: ${({ theme }) => `${theme.space[4]}px`};
+  display: none;
+  
+  @media (min-width: 1240px) {
+    display: block;
+    font-family: ${({ theme }) => `${theme.fonts.main}`};
+    font-style: normal;
+    font-weight: ${({ theme }) => `${theme.fontWeights.regular}`};
+    font-size: 14px;
+    line-height: ${({ theme }) => `${theme.lineHeights.heading}`};
+    letter-spacing: -0.02em;
+    color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
+    margin-bottom: ${({ theme }) => `${theme.space[4]}px`};
+  }
 `;
 
 export const EmailInput = styled.input`
-  width: 338px;
-  height: 59px;
+  width: 206px;
+  height: 38px;
   background-color: transparent;
-  padding-top: 18px;
-  padding-bottom: 18px;
-  padding-left: 52px;
-  padding-right: 71px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 42px;
+  padding-right: 42px;
+  margin-top: 32px;
   border: 1px solid #fafafa;
-  border-radius: 5px;
+  border-radius: 6px;
   font-family: ${({ theme }) => `${theme.fonts.main}`};
   font-style: normal;
   font-weight: ${({ theme }) => `${theme.fontWeights.regular}`};
-  font-size: 18px;
+  font-size: 10px;
   line-height: ${({ theme }) => `${theme.lineHeights.large}`};
   letter-spacing: -0.02em;
-  margin-bottom: 16px;
+  margin-bottom: 8px;
+  margin-right: auto;
+  margin-left: auto;
+  display: block;
   cursor: pointer;
   color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
   ::placeholder {
     color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
+  }
+  @media (min-width: 768px) {
+    margin-top: 0px;
+    width: 259px;
+    height: 50px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    padding-left: 51px;
+    padding-right: 40px;
+    font-size: 14px;
+    line-height: ${({ theme }) => `${theme.lineHeights.large}`};
+    margin-bottom:0;
+  }
+  @media (min-width: 1240px) {
+    width: 339px;
+    height: 59px;
+    padding-top: 18px;
+    padding-bottom: 18px;
+    padding-left: 52px;
+    padding-right: 71px;
+    margin-bottom: 16px;
+    font-size: 18px;
   }
 `;
 
@@ -149,20 +278,36 @@ export const MailIcon = styled(MdOutlineEmail)`
   width: 20px;
   height: 20px;
   color: ${({ theme }) => `${theme.colors.mainLight}`};
-  top: 20px;
-  left: 20px;
+  top: 8px;
+  left: 75px;
   cursor: pointer;
+  @media (min-width: 768px) {
+    left: 20px;
+    top: 15px;
+  }
+  @media (min-width: 1240px) {
+    top: 20px;
+    left: 20px;
+  }
 `;
 export const MailBtn = styled.button`
+  font-family: ${({ theme }) => `${theme.fonts.main}`};
+  font-style: normal;
+  font-weight: ${({ theme }) => `${theme.fontWeights.regular}`};
+  font-size: 14px;
+  line-height: ${({ theme }) => `${theme.lineHeights.single}`};
+  text-align: center;
   display: flex;
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: 46px;
   cursor: pointer;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  padding: 21px 138px;
-  gap: 8px;
-  width: 339px;
-  height: 60px;
+  padding: 11px 71px;
+  width: 206px;
+  height: 38px;
   background: ${({ theme }) => `${theme.colors.accent}`};
   border-radius: ${({ theme }) => `${theme.radius.small}`};
   border: none;
@@ -172,6 +317,19 @@ export const MailBtn = styled.button`
     transition: all 0.5s ease-in-out;
     color: ${({ theme }) => `${theme.colors.accent}`};
     background: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
+  }
+  @media (min-width: 768px) {
+    margin: 0;
+    width: 171px;
+    height: 50px;
+    font-size: 16px;
+  }
+  @media (min-width: 1240px) {
+    width: 339px;
+    height: 60px;
+    padding: 21px 138px;
+    font-size: 16px;
+    margin-bottom: 0;
   }
 `;
 export const FacebookIcon = styled(SiFacebook)`
@@ -184,7 +342,6 @@ export const FacebookIcon = styled(SiFacebook)`
     transition: all 0.25s ease-in-out;
     color: ${({ theme }) => `${theme.colors.backgroundPrimary}`};
   }
-  
 `;
 
 export const YTIcon = styled(SiYoutube)`
@@ -226,8 +383,49 @@ export const InstIcon = styled(SiInstagram)`
 export const SocialContainer = styled.ul`
   display: flex;
   justify-content: center;
-  margin-top: 40px;
-  padding-bottom: 50px;
+  margin-top: 0;
+  /* padding-bottom: 20px; */
   gap: 20px;
-  
+  @media (min-width: 1240px) {
+    padding-bottom: 50px;
+    margin-top: 40px;
+  }
+`;
+
+export const Copyright = styled.div`
+  margin-top: 28px;
+  display: flex;
+  justify-content: center;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 10px;
+  letter-spacing: -0.01em;
+  color: #22252a;
+  opacity: 0.5;
+  @media (min-width: 1240px) {
+    margin-top: 50px;
+    padding-bottom: 50px;
+    margin-top: 40px;
+    font-size: 14px;
+    line-height: 14px;
+  }
+`;
+
+export const CopyrightText = styled.span`
+  margin-left: 15px;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 10px;
+  letter-spacing: -0.01em;
+  color: #22252a;
+  opacity: 0.5;
+  @media (min-width: 1240px) {
+    font-size: 14px;
+    line-height: 14px;
+    margin-left: 40px;
+  }
 `;
