d2ddf7fda2a7957385e247964a5b894e6e29e901
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-09 18:03:13 +0300

    Update FavoritesPage.jsx

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectFavoriteRecipes } from "redux/favorite/selectors";
