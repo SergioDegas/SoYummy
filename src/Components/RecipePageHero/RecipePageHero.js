1b52911a4286b66f4ca7b36847ebe6ed6a9464c1
Sergey Miniaylo <po4tovuj@gmail.com>
2023-04-10 16:48:52 +0300

    comments unused vars

--- src/Components/RecipePageHero/RecipePageHero.js
+++ src/Components/RecipePageHero/RecipePageHero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from 'redux/auth/selectors';
+import { useDispatch } from 'react-redux';
+// import { selectUser } from 'redux/auth/selectors';
 // import { selectFavoriteRecipes } from "redux/favorite/selectors";
 import Container from '../Container';
 import { addToFavoriteList } from 'redux/favorite/operations';
