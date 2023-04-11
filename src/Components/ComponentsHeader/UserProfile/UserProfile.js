3be7eda67bfc01196b1d79d3b578d7709599fd1f
Viktor Annyenkov <annyenkov@gmail.com>
2023-04-11 23:28:08 +0300

    Update UserProfile.js

--- src/Components/ComponentsHeader/UserProfile/UserProfile.js
+++ src/Components/ComponentsHeader/UserProfile/UserProfile.js
@@ -16,20 +16,21 @@ import {
 
 export const UserProfile = ({ onClose, photoUrl, userName }) => {
   const [newName, setNewName] = useState(userName);
-  const [file, setFile] = useState(photoUrl);
+  const [avatar, setAvatar] = useState(photoUrl);
+  const [file, setFile] = useState()
 
   const uploadContent = (e) => {
     if (e.target.files[0]) {
       const objectUrl = URL.createObjectURL(e.target.files[0]);
-
-      setFile(objectUrl);
+      setAvatar(objectUrl);
+      setFile(e.target.files[0])
     }
   };
 
   const handleChange = async () => {
     const formData = new FormData();
-    formData.append("name", newName);
     formData.append("image", file);
+    formData.append("name", newName);
 
     try {
       await setUserData(formData);
@@ -40,7 +41,7 @@ export const UserProfile = ({ onClose, photoUrl, userName }) => {
 
   return (
     <EditContainer>
-      <CircleImage style={{ backgroundImage: `url(${file})` }}>
+      <CircleImage style={{ backgroundImage: `url(${avatar})` }}>
         <AddPhotoButton>
           <AiOutlinePlus
             style={{
