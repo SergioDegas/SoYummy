3aee399ac92b2367f08a73aa9720a3b2997faf95
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-11 23:53:13 +0300

    Update operation.js

--- src/redux/recipes/operation.js
+++ src/redux/recipes/operation.js
@@ -1,42 +1,39 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-
 export const fetchRecipes = createAsyncThunk(
-
-	'user/own-recipes',
-	async (_, thunkAPI) => {
-		try {
-			const {result} = await axios.get('user/own-recipes');
-			return result.data;
-		
-		} catch (error) {
-			return thunkAPI.rejectWithValue(error.message)
-		};
-	}
+    "user/own-recipes",
+    async (_, thunkAPI) => {
+        try {
+            const { data } = await axios.get("user/own-recipes");
+            console.log(data);
+            return data.result;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
 );
 
 export const addRecipe = createAsyncThunk(
-	'add/own-recipes',
-	async (credentials, thunkAPI) => {
-		try {
-			const {data} = await axios.post('/recipes',credentials);
-			return data;
-		} catch (error) {
-			return thunkAPI.rejectWithValue(error.message)
-		};
-	}
+    "add/own-recipes",
+    async (credentials, thunkAPI) => {
+        try {
+            const { data } = await axios.post("/recipes", credentials);
+            return data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
 );
 
 export const removeRecipe = createAsyncThunk(
-	'remove/own-recipes',
-	async (recipeId, thunkApi) => {
-		try {
-			const {data} = await axios.delete(`/recipes/${recipeId}`);
-			return data;
-		} catch (error) {
-			return thunkApi.rejectWithValue(error.message)
-		};
-	}
-
+    "remove/own-recipes",
+    async (recipeId, thunkApi) => {
+        try {
+            const { data } = await axios.delete(`/recipes/${recipeId}`);
+            return data;
+        } catch (error) {
+            return thunkApi.rejectWithValue(error.message);
+        }
+    }
 );
