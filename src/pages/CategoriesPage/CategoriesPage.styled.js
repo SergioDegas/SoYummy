c22cbfb64943a91fbb43a1b04d0b3d3687c80b2a
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-11 17:35:03 +0300

    styles fix + loader added

--- src/pages/CategoriesPage/CategoriesPage.styled.js
+++ src/pages/CategoriesPage/CategoriesPage.styled.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-    padding-top: ${(p) => p.theme.space[1] * 12.5}px;
+    padding-top: ${(p) => p.theme.space[1] * 28.5}px;
     padding-bottom: ${(p) => p.theme.space[1] * 25}px;
 
     @media (min-width: 768px) {
-        padding-top: ${(p) => p.theme.space[1] * 17.5}px;
+        padding-top: ${(p) => p.theme.space[1] * 34}px;
         padding-bottom: ${(p) => p.theme.space[1] * 50}px;
     }
 
     @media (min-width: 1440px) {
-        padding-top: ${(p) => p.theme.space[1] * 25}px;
+        padding-top: ${(p) => p.theme.space[1] * 41}px;
     }
 `;
 
